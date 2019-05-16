5cb2a8331408e2eb74888ab2b8df0633ba44f848
Ben Grout <ben@bgrout.co.uk>
2019-05-16 18:37:36 +0100

    implement ajax, init when data loaded, find min/max teams

--- slider.js
+++ slider.js
@@ -1,43 +1,39 @@
 /*! rangeslider.js - v2.3.2 | (c) 2018 @andreruffert | MIT license | https://github.com/andreruffert/rangeslider.js */
 !function(a){"use strict";"function"==typeof define&&define.amd?define(["jquery"],a):"object"==typeof exports?module.exports=a(require("jquery")):a(jQuery)}(function(a){"use strict";function b(){var a=document.createElement("input");return a.setAttribute("type","range"),"text"!==a.type}function c(a,b){var c=Array.prototype.slice.call(arguments,2);return setTimeout(function(){return a.apply(null,c)},b)}function d(a,b){return b=b||100,function(){if(!a.debouncing){var c=Array.prototype.slice.apply(arguments);a.lastReturnVal=a.apply(window,c),a.debouncing=!0}return clearTimeout(a.debounceTimeout),a.debounceTimeout=setTimeout(function(){a.debouncing=!1},b),a.lastReturnVal}}function e(a){return a&&(0===a.offsetWidth||0===a.offsetHeight||!1===a.open)}function f(a){for(var b=[],c=a.parentNode;e(c);)b.push(c),c=c.parentNode;return b}function g(a,b){function c(a){void 0!==a.open&&(a.open=!a.open)}var d=f(a),e=d.length,g=[],h=a[b];if(e){for(var i=0;i<e;i++)g[i]=d[i].style.cssText,d[i].style.setProperty?d[i].style.setProperty("display","block","important"):d[i].style.cssText+=";display: block !important",d[i].style.height="0",d[i].style.overflow="hidden",d[i].style.visibility="hidden",c(d[i]);h=a[b];for(var j=0;j<e;j++)d[j].style.cssText=g[j],c(d[j])}return h}function h(a,b){var c=parseFloat(a);return Number.isNaN(c)?b:c}function i(a){return a.charAt(0).toUpperCase()+a.substr(1)}function j(b,e){if(this.$window=a(window),this.$document=a(document),this.$element=a(b),this.options=a.extend({},n,e),this.polyfill=this.options.polyfill,this.orientation=this.$element[0].getAttribute("data-orientation")||this.options.orientation,this.onInit=this.options.onInit,this.onSlide=this.options.onSlide,this.onSlideEnd=this.options.onSlideEnd,this.DIMENSION=o.orientation[this.orientation].dimension,this.DIRECTION=o.orientation[this.orientation].direction,this.DIRECTION_STYLE=o.orientation[this.orientation].directionStyle,this.COORDINATE=o.orientation[this.orientation].coordinate,this.polyfill&&m)return!1;this.identifier="js-"+k+"-"+l++,this.startEvent=this.options.startEvent.join("."+this.identifier+" ")+"."+this.identifier,this.moveEvent=this.options.moveEvent.join("."+this.identifier+" ")+"."+this.identifier,this.endEvent=this.options.endEvent.join("."+this.identifier+" ")+"."+this.identifier,this.toFixed=(this.step+"").replace(".","").length-1,this.$fill=a('<div class="'+this.options.fillClass+'" />'),this.$handle=a('<div class="'+this.options.handleClass+'" />'),this.$range=a('<div class="'+this.options.rangeClass+" "+this.options[this.orientation+"Class"]+'" id="'+this.identifier+'" />').insertAfter(this.$element).prepend(this.$fill,this.$handle),this.$element.css({position:"absolute",width:"1px",height:"1px",overflow:"hidden",opacity:"0"}),this.handleDown=a.proxy(this.handleDown,this),this.handleMove=a.proxy(this.handleMove,this),this.handleEnd=a.proxy(this.handleEnd,this),this.init();var f=this;this.$window.on("resize."+this.identifier,d(function(){c(function(){f.update(!1,!1)},300)},20)),this.$document.on(this.startEvent,"#"+this.identifier+":not(."+this.options.disabledClass+")",this.handleDown),this.$element.on("change."+this.identifier,function(a,b){if(!b||b.origin!==f.identifier){var c=a.target.value,d=f.getPositionFromValue(c);f.setPosition(d)}})}Number.isNaN=Number.isNaN||function(a){return"number"==typeof a&&a!==a};var k="rangeslider",l=0,m=b(),n={polyfill:!0,orientation:"horizontal",rangeClass:"rangeslider",disabledClass:"rangeslider--disabled",activeClass:"rangeslider--active",horizontalClass:"rangeslider--horizontal",verticalClass:"rangeslider--vertical",fillClass:"rangeslider__fill",handleClass:"rangeslider__handle",startEvent:["mousedown","touchstart","pointerdown"],moveEvent:["mousemove","touchmove","pointermove"],endEvent:["mouseup","touchend","pointerup"]},o={orientation:{horizontal:{dimension:"width",direction:"left",directionStyle:"left",coordinate:"x"},vertical:{dimension:"height",direction:"top",directionStyle:"bottom",coordinate:"y"}}};return j.prototype.init=function(){this.update(!0,!1),this.onInit&&"function"==typeof this.onInit&&this.onInit()},j.prototype.update=function(a,b){a=a||!1,a&&(this.min=h(this.$element[0].getAttribute("min"),0),this.max=h(this.$element[0].getAttribute("max"),100),this.value=h(this.$element[0].value,Math.round(this.min+(this.max-this.min)/2)),this.step=h(this.$element[0].getAttribute("step"),1)),this.handleDimension=g(this.$handle[0],"offset"+i(this.DIMENSION)),this.rangeDimension=g(this.$range[0],"offset"+i(this.DIMENSION)),this.maxHandlePos=this.rangeDimension-this.handleDimension,this.grabPos=this.handleDimension/2,this.position=this.getPositionFromValue(this.value),this.$element[0].disabled?this.$range.addClass(this.options.disabledClass):this.$range.removeClass(this.options.disabledClass),this.setPosition(this.position,b)},j.prototype.handleDown=function(a){if(a.preventDefault(),!(a.button&&0!==a.button||(this.$document.on(this.moveEvent,this.handleMove),this.$document.on(this.endEvent,this.handleEnd),this.$range.addClass(this.options.activeClass),(" "+a.target.className+" ").replace(/[\n\t]/g," ").indexOf(this.options.handleClass)>-1))){var b=this.getRelativePosition(a),c=this.$range[0].getBoundingClientRect()[this.DIRECTION],d=this.getPositionFromNode(this.$handle[0])-c,e="vertical"===this.orientation?this.maxHandlePos-(b-this.grabPos):b-this.grabPos;this.setPosition(e),b>=d&&b<d+this.handleDimension&&(this.grabPos=b-d)}},j.prototype.handleMove=function(a){a.preventDefault();var b=this.getRelativePosition(a),c="vertical"===this.orientation?this.maxHandlePos-(b-this.grabPos):b-this.grabPos;this.setPosition(c)},j.prototype.handleEnd=function(a){a.preventDefault(),this.$document.off(this.moveEvent,this.handleMove),this.$document.off(this.endEvent,this.handleEnd),this.$range.removeClass(this.options.activeClass),this.$element.trigger("change",{origin:this.identifier}),this.onSlideEnd&&"function"==typeof this.onSlideEnd&&this.onSlideEnd(this.position,this.value)},j.prototype.cap=function(a,b,c){return a<b?b:a>c?c:a},j.prototype.setPosition=function(a,b){var c,d;void 0===b&&(b=!0),c=this.getValueFromPosition(this.cap(a,0,this.maxHandlePos)),d=this.getPositionFromValue(c),this.$fill[0].style[this.DIMENSION]=d+this.grabPos+"px",this.$handle[0].style[this.DIRECTION_STYLE]=d+"px",this.setValue(c),this.position=d,this.value=c,b&&this.onSlide&&"function"==typeof this.onSlide&&this.onSlide(d,c)},j.prototype.getPositionFromNode=function(a){for(var b=0;null!==a;)b+=a.offsetLeft,a=a.offsetParent;return b},j.prototype.getRelativePosition=function(a){var b=i(this.COORDINATE),c=this.$range[0].getBoundingClientRect()[this.DIRECTION],d=0;return void 0!==a.originalEvent["client"+b]?d=a.originalEvent["client"+b]:a.originalEvent.touches&&a.originalEvent.touches[0]&&void 0!==a.originalEvent.touches[0]["client"+b]?d=a.originalEvent.touches[0]["client"+b]:a.currentPoint&&void 0!==a.currentPoint[this.COORDINATE]&&(d=a.currentPoint[this.COORDINATE]),d-c},j.prototype.getPositionFromValue=function(a){var b;return b=(a-this.min)/(this.max-this.min),Number.isNaN(b)?0:b*this.maxHandlePos},j.prototype.getValueFromPosition=function(a){var b,c;return b=a/(this.maxHandlePos||1),c=this.step*Math.round(b*(this.max-this.min)/this.step)+this.min,Number(c.toFixed(this.toFixed))},j.prototype.setValue=function(a){a===this.value&&""!==this.$element[0].value||this.$element.val(a).trigger("input",{origin:this.identifier})},j.prototype.destroy=function(){this.$document.off("."+this.identifier),this.$window.off("."+this.identifier),this.$element.off("."+this.identifier).removeAttr("style").removeData("plugin_"+k),this.$range&&this.$range.length&&this.$range[0].parentNode.removeChild(this.$range[0])},a.fn[k]=function(b){var c=Array.prototype.slice.call(arguments,1);return this.each(function(){var d=a(this),e=d.data("plugin_"+k);e||d.data("plugin_"+k,e=new j(this,b)),"string"==typeof b&&e[b].apply(e,c)})},"rangeslider.js is available in jQuery context e.g $(selector).rangeslider(options);"});
 
+window.rangeSliderVal = 0;
+window.priceBands = {};
 
-//custom slider javascript
-var $element = $('#emm-slider');
-var $handle;
+function initSlider(priceBands) {
+	//custom slider javascript
+	var $element = $('#emm-slider');
+	var $handle;
 
-//get json, on complete:
-// - find min of lowest band and max of highest band
-// - update min/max of range element
-// - initialise rangeslider
-
-$element
-  .rangeslider({
-    polyfill: false,
-    onInit: function() {
-      $handle = $('.rangeslider__handle', this.$range);
-      updateHandle($handle[0], this.value);
-      updatePrice(this.value);
-    }
-  })
-  .on('input', function() {
-    updateHandle($handle[0], this.value);
-    updatePrice(this.value);
-  });
+	$element.rangeslider({
+		polyfill: false,
+	 	onInit: function() {
+			$handle = $('.rangeslider__handle', this.$range);
+			updateHandle($handle[0], this.value);
+	 		updatePrice(this.value, priceBands);
+		}
+	  })
+	.on('input', function() {
+		updateHandle($handle[0], this.value);
+	 	updatePrice(this.value, priceBands);
+	});	
+}
 
 function updateHandle(el, val) {
   $(el).html('<span class="num-teams">' + val + '</span>' + " Teams");
 }
 
-function updatePrice(val) {
+function updatePrice(val, priceBands) {
 	window.rangeSliderVal = val;
-	var priceBands = {
-	    "data": {"cup":{"bands":[{"id":1,"type":"cup","min_teams":2,"max_teams":10,"price":100,"advanced_price":50},{"id":2,"type":"cup","min_teams":11,"max_teams":20,"price":200,"advanced_price":50},{"id":3,"type":"cup","min_teams":21,"max_teams":30,"price":300,"advanced_price":100},{"id":4,"type":"cup","min_teams":31,"max_teams":40,"price":400,"advanced_price":200},{"id":5,"type":"cup","min_teams":41,"max_teams":50,"price":500,"advanced_price":300},{"id":6,"type":"cup","min_teams":51,"max_teams":60,"price":600,"advanced_price":400}]},"league":{"bands":[{"id":7,"type":"league","min_teams":2,"max_teams":10,"price":200,"advanced_price":null},{"id":8,"type":"league","min_teams":11,"max_teams":20,"price":300,"advanced_price":null},{"id":9,"type":"league","min_teams":21,"max_teams":30,"price":400,"advanced_price":null},{"id":10,"type":"league","min_teams":31,"max_teams":40,"price":500,"advanced_price":null},{"id":11,"type":"league","min_teams":41,"max_teams":50,"price":600,"advanced_price":null},{"id":12,"type":"league","min_teams":51,"max_teams":60,"price":700,"advanced_price":null}]}},
-	    "status_code": "200"
-	};
+
 	const result = priceBands.data.cup.bands.filter(filterByTeams);
 
-	var foundPrice = "Please call";
+	//Default button text when price not found
+	var foundPrice = "Get in touch for prices";
 
 	if(result.length > 0) {
 		foundPrice = "From €" + result[0].price;
@@ -56,10 +52,30 @@ function filterByTeams(item) {
 	return false;
 }
 
-window.rangeSliderVal = 0;
+function priceMinMax(priceBands) {
+	var min = Infinity, max = -Infinity, i, band;
+	var bands = priceBands.data.cup.bands;
+
+	for( i in bands) {
+		band = bands[i];
+	    if( band.min_teams < min) min = band.min_teams;
+	    if( band.max_teams > max) max = band.max_teams;
+	}
+
+
+	return {min: min, max: max};
+}
 
 $(document).ready(function(){
-	$.getJSON('https://comm-qa.wot.esrtmp.com/api/v1/tournament-pricing-bands', function(data){
-		console.log(data);
+	var apiUrl = 'https://raw.githubusercontent.com/bengrout/emm-pricing-slider/master/tournament-pricing.bands.json';
+	$.getJSON(apiUrl, function(data){
+		var minMax = priceMinMax(data);
+		
+		$('#emm-slider').attr({
+			"min": minMax.min,
+			"max": minMax.max
+		});
+
+		initSlider(data);
 	});
 });
